5902b7e0f1dcb32b263575bc8efcfd8281e8b07a
sowmik-sec <sowmik.sec@gmail.com>
2024-08-28 22:14:04 +0600

    Display different menu category items with cover

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,14 +1,39 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../shared/Cover/Cover";
 import menuImg from "../../../assets/menu/banner3.jpg";
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg";
+import soupImg from "../../../assets/menu/soup-bg.jpg";
+import saladImg from "../../../assets/menu/salad-bg.jpg";
+import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 function Menu() {
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
+      {/* main cover */}
       <Cover img={menuImg} title={"Our menu"} />
+      <SectionTitle subHeading={`Don't Miss`} heading={`Today's offer`} />
+      {/* offered menu items */}
+      <MenuCategory items={offered} />
+      {/*  dessert menu items*/}
+      <MenuCategory items={dessert} title={"Dessert"} img={dessertImg} />
+      {/* Pizza */}
+      <MenuCategory items={pizza} title={"Pizza"} img={pizzaImg} />
+      {/* Soup */}
+      <MenuCategory items={soup} title={"Soup"} img={soupImg} />
+      {/* Salad */}
+      <MenuCategory items={salad} title={"Salad"} img={saladImg} />
     </div>
   );
 }
--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,5 +1,17 @@
-function MenuCategory() {
-  return <div></div>;
+import Cover from "../../shared/Cover/Cover";
+import MenuItem from "../../shared/MenuItem/MenuItem";
+
+function MenuCategory({ items, title, img }) {
+  return (
+    <div className="pt-8">
+      {title && <Cover img={img} title={title} />}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 my-16">
+        {items.map((menu) => (
+          <MenuItem key={menu._id} item={menu} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default MenuCategory;

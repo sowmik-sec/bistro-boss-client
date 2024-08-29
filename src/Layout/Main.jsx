bc1e2ad04d6f2e5c3ba3597f46b2cd2835dd1de4
sowmik-sec <sowmik.sec@gmail.com>
2024-08-29 10:24:12 +0600

    make layout functional for login page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/shared/Footer/Footer";
 import Navbar from "../pages/shared/Navbar/Navbar";
 
 function Main() {
+  const location = useLocation();
+  console.log(location);
+  const noHeaderFooter = location.pathname.includes("login");
   return (
     <div>
-      <Navbar />
+      {noHeaderFooter || <Navbar />}
       <Outlet />
-      <Footer />
+      {noHeaderFooter || <Footer />}
     </div>
   );
 }

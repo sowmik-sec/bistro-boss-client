cd5af6d2b646dab29b544c8e13e793e24805caaf
sowmik-sec <sowmik.sec@gmail.com>
2024-08-29 09:17:16 +0600

    Load menu data from server

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -9,11 +9,13 @@ function FoodCard({ item }) {
       <p className="bg-slate-900 text-white absolute right-0 mr-4 mt-4 px-4">
         ${price}
       </p>
-      <div className="card-body text-center">
+      <div className="card-body text-center flex flex-col items-center">
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Add to Cart</button>
+          <button className="btn btn-outline border-orange-400 bg-slate-100 border-0 border-b-4 mt-4">
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>

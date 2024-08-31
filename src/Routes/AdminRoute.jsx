f67f701cd8820bb71844f90d5bb5a3e827834346
sowmik-sec <sowmik.sec@gmail.com>
2024-08-31 19:48:15 +0600

    create admin route

---
+++ src/Routes/AdminRoute.jsx
@@ -0,0 +1,22 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../hooks/useAdmin";
+import useAuth from "../hooks/useAuth";
+
+function AdminRoute({ children }) {
+  const { user, loading } = useAuth();
+  const [isAdmin, isAdminLoading] = useAdmin();
+  const location = useLocation();
+  if (loading || isAdminLoading) {
+    return (
+      <div>
+        <progress className="progress w-56"></progress>
+      </div>
+    );
+  }
+  if (user && isAdmin) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} replace />;
+}
+
+export default AdminRoute;

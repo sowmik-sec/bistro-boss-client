5f08b9130761fc9e02b97ecc1636d2e543fd593d
sowmik-sec <sowmik.sec@gmail.com>
2024-09-01 22:45:55 +0600

    create useMenu hook using tanstack query

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,17 +1,31 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "./useAxiosPublic";
 
 const useMenu = () => {
-  const [menu, setMenu] = useState([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    fetch("http://localhost:5000/menu")
-      .then((res) => res.json())
-      .then((data) => {
-        setMenu(data);
-        setLoading(false);
-      })
-      .catch((err) => console.error(err));
-  }, []);
-  return [menu, loading];
+  // const [menu, setMenu] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/menu")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setMenu(data);
+  //       setLoading(false);
+  //     })
+  //     .catch((err) => console.error(err));
+  // }, []);
+  // return [menu, loading];
+  const axiosPublic = useAxiosPublic();
+  const {
+    data: menu = [],
+    refetch,
+    isPending: loading,
+  } = useQuery({
+    queryKey: ["menu"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("menu");
+      return res.data;
+    },
+  });
+  return [menu, refetch, loading];
 };
 export default useMenu;

dd7e82f3fc0178735421d413fbec6d1169fa0104
sowmik-sec <sowmik.sec@gmail.com>
2024-09-02 18:16:42 +0600

    Load data and Display stats on the admin home

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,12 +1,59 @@
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaBook, FaDollarSign, FaList, FaUsers } from "react-icons/fa";
 
 function AdminHome() {
   const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: stats } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-stats");
+      return res.data;
+    },
+  });
   return (
     <div>
       <h2 className="text-3xl">
         Hi, Welcome {user?.displayName ? user.displayName : "Back"}
       </h2>
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaDollarSign className="text-3xl" />
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">${stats?.revenue}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers className="text-3xl" />
+          </div>
+          <div className="stat-title">Users</div>
+          <div className="stat-value">{stats?.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaBook className="text-3xl" />
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats?.orders}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaList className="text-3xl" />
+          </div>
+          <div className="stat-title">Menu Items</div>
+          <div className="stat-value">{stats?.menuItems}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
     </div>
   );
 }

2d3ca70c93f935c7083675ba2d1a8a7cc6a01829
sowmik-sec <sowmik.sec@gmail.com>
2024-09-02 17:51:35 +0600

    Fix Login issue and API race condition

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -55,13 +55,14 @@ function AuthProvider({ children }) {
         axiosPublic.post("/jwt", userInfo).then((res) => {
           if (res.data.token) {
             localStorage.setItem("access-token", res.data.token);
+            setLoading(false);
           }
         });
       } else {
         // remove token(if token stored in the client side: local storage, caching, in memory)
         localStorage.removeItem("access-token");
+        setLoading(false);
       }
-      setLoading(false);
     });
     return () => {
       return unsubscribe();

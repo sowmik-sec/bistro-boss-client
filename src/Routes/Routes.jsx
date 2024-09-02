7ab9ac622e39111d492a16676e22918e664d49ac
sowmik-sec <sowmik.sec@gmail.com>
2024-09-02 20:53:31 +0600

    deploy website to firebase

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -112,7 +112,7 @@ export const router = createBrowserRouter([
           </AdminRoute>
         ),
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/menu/${params.id}`),
+          fetch(`https://bistro-boss-alpha.vercel.app/menu/${params.id}`),
       },
       // admin users
       {

10ceb87389f9b560ebd32a93a54ecc9261ab0b0a
sowmik-sec <sowmik.sec@gmail.com>
2024-09-02 11:03:40 +0600

    Use Confirm Card Payment to Process Payment

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -2,13 +2,16 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useCart from "../../../hooks/useCart";
+import useAuth from "../../../hooks/useAuth";
 
 function CheckoutForm() {
   const stripe = useStripe();
   const elements = useElements();
   const [error, setError] = useState("");
   const [clientSecret, setClientSecret] = useState("");
+  const [transactionId, setTransactionId] = useState("");
   const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
   const [cart] = useCart();
   const totalPrice = cart.reduce((total, item) => total + item.price, 0);
   useEffect(() => {
@@ -45,6 +48,25 @@ function CheckoutForm() {
       console.log("[PaymentMethod]", paymentMethod);
       setError("");
     }
+    // confirm payment
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || "anonymous",
+            name: user?.displayName || "anonymous",
+          },
+        },
+      });
+    if (confirmError) {
+      console.log("confirm error");
+    } else {
+      console.log("payment intent", paymentIntent);
+      if (paymentIntent.status === "succeeded") {
+        setTransactionId(paymentIntent.id);
+      }
+    }
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -72,6 +94,7 @@ function CheckoutForm() {
         Pay
       </button>
       <p className="text-red-400">{error}</p>
+      {transactionId && <p>Your transaction id: {transactionId}</p>}
     </form>
   );
 }
